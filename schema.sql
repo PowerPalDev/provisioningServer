5ba833e216cfed9f83119c9c78b0e05496a1e48b
Roy Bellingan <admin@seisho.us>
2024-10-23 02:04:29 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -18,16 +18,16 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: backend; Type: SCHEMA; Schema: -; Owner: roy
+-- Name: backend; Type: SCHEMA; Schema: -; Owner: digitalspine
 --
 
 CREATE SCHEMA backend;
 
 
-ALTER SCHEMA backend OWNER TO roy;
+ALTER SCHEMA backend OWNER TO digitalspine;
 
 --
--- Name: applianceType; Type: TYPE; Schema: backend; Owner: roy
+-- Name: applianceType; Type: TYPE; Schema: backend; Owner: digitalspine
 --
 
 CREATE TYPE backend."applianceType" AS ENUM (
@@ -47,10 +47,10 @@ CREATE TYPE backend."applianceType" AS ENUM (
 );
 
 
-ALTER TYPE backend."applianceType" OWNER TO roy;
+ALTER TYPE backend."applianceType" OWNER TO digitalspine;
 
 --
--- Name: deviceType; Type: TYPE; Schema: backend; Owner: roy
+-- Name: deviceType; Type: TYPE; Schema: backend; Owner: digitalspine
 --
 
 CREATE TYPE backend."deviceType" AS ENUM (
@@ -59,14 +59,56 @@ CREATE TYPE backend."deviceType" AS ENUM (
 );
 
 
-ALTER TYPE backend."deviceType" OWNER TO roy;
+ALTER TYPE backend."deviceType" OWNER TO digitalspine;
+
+--
+-- Name: user_id_seq; Type: SEQUENCE; Schema: backend; Owner: digitalspine
+--
+
+CREATE SEQUENCE backend.user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 2147483647
+    CACHE 1;
+
+
+ALTER SEQUENCE backend.user_id_seq OWNER TO digitalspine;
 
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: admin; Type: TABLE; Schema: backend; Owner: roy
+-- Name: account; Type: TABLE; Schema: backend; Owner: digitalspine
+--
+
+CREATE TABLE backend.account (
+    id integer DEFAULT nextval('backend.user_id_seq'::regclass) NOT NULL,
+    name character(255) NOT NULL,
+    password character(255) NOT NULL,
+    "devicePassword" character(255) NOT NULL
+);
+
+
+ALTER TABLE backend.account OWNER TO digitalspine;
+
+--
+-- Name: COLUMN account.password; Type: COMMENT; Schema: backend; Owner: digitalspine
+--
+
+COMMENT ON COLUMN backend.account.password IS 'this is the password the user chooses to login in the web panel etc, is hashed salt and peppered etc';
+
+
+--
+-- Name: COLUMN account."devicePassword"; Type: COMMENT; Schema: backend; Owner: digitalspine
+--
+
+COMMENT ON COLUMN backend.account."devicePassword" IS 'this is the one we send to the device to login, for now we store in clear, later will be encrypted (not hashed)';
+
+
+--
+-- Name: admin; Type: TABLE; Schema: backend; Owner: digitalspine
 --
 
 CREATE TABLE backend.admin (
@@ -77,24 +119,24 @@ CREATE TABLE backend.admin (
 );
 
 
-ALTER TABLE backend.admin OWNER TO roy;
+ALTER TABLE backend.admin OWNER TO digitalspine;
 
 --
--- Name: TABLE admin; Type: COMMENT; Schema: backend; Owner: roy
+-- Name: TABLE admin; Type: COMMENT; Schema: backend; Owner: digitalspine
 --
 
 COMMENT ON TABLE backend.admin IS 'this table is used to manage the user able to login to the panel / use the api';
 
 
 --
--- Name: COLUMN admin.password; Type: COMMENT; Schema: backend; Owner: roy
+-- Name: COLUMN admin.password; Type: COMMENT; Schema: backend; Owner: digitalspine
 --
 
 COMMENT ON COLUMN backend.admin.password IS 'for the moment a sha1 hash, later we might use something better';
 
 
 --
--- Name: admin_id_seq; Type: SEQUENCE; Schema: backend; Owner: roy
+-- Name: admin_id_seq; Type: SEQUENCE; Schema: backend; Owner: digitalspine
 --
 
 CREATE SEQUENCE backend.admin_id_seq
@@ -106,17 +148,17 @@ CREATE SEQUENCE backend.admin_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE backend.admin_id_seq OWNER TO roy;
+ALTER SEQUENCE backend.admin_id_seq OWNER TO digitalspine;
 
 --
--- Name: admin_id_seq; Type: SEQUENCE OWNED BY; Schema: backend; Owner: roy
+-- Name: admin_id_seq; Type: SEQUENCE OWNED BY; Schema: backend; Owner: digitalspine
 --
 
 ALTER SEQUENCE backend.admin_id_seq OWNED BY backend.admin.id;
 
 
 --
--- Name: appliance_id_seq; Type: SEQUENCE; Schema: backend; Owner: roy
+-- Name: appliance_id_seq; Type: SEQUENCE; Schema: backend; Owner: digitalspine
 --
 
 CREATE SEQUENCE backend.appliance_id_seq
@@ -127,10 +169,10 @@ CREATE SEQUENCE backend.appliance_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE backend.appliance_id_seq OWNER TO roy;
+ALTER SEQUENCE backend.appliance_id_seq OWNER TO digitalspine;
 
 --
--- Name: appliance; Type: TABLE; Schema: backend; Owner: roy
+-- Name: appliance; Type: TABLE; Schema: backend; Owner: digitalspine
 --
 
 CREATE TABLE backend.appliance (
@@ -148,10 +190,10 @@ CREATE TABLE backend.appliance (
 );
 
 
-ALTER TABLE backend.appliance OWNER TO roy;
+ALTER TABLE backend.appliance OWNER TO digitalspine;
 
 --
--- Name: smartPlug; Type: TABLE; Schema: backend; Owner: roy
+-- Name: smartPlug; Type: TABLE; Schema: backend; Owner: digitalspine
 --
 
 CREATE TABLE backend."smartPlug" (
@@ -161,24 +203,24 @@ CREATE TABLE backend."smartPlug" (
 );
 
 
-ALTER TABLE backend."smartPlug" OWNER TO roy;
+ALTER TABLE backend."smartPlug" OWNER TO digitalspine;
 
 --
--- Name: COLUMN "smartPlug"."maxPower"; Type: COMMENT; Schema: backend; Owner: roy
+-- Name: COLUMN "smartPlug"."maxPower"; Type: COMMENT; Schema: backend; Owner: digitalspine
 --
 
 COMMENT ON COLUMN backend."smartPlug"."maxPower" IS 'used for sim';
 
 
 --
--- Name: COLUMN "smartPlug"."minPower"; Type: COMMENT; Schema: backend; Owner: roy
+-- Name: COLUMN "smartPlug"."minPower"; Type: COMMENT; Schema: backend; Owner: digitalspine
 --
 
 COMMENT ON COLUMN backend."smartPlug"."minPower" IS 'used for sim';
 
 
 --
--- Name: smartplugview; Type: VIEW; Schema: backend; Owner: roy
+-- Name: smartplugview; Type: VIEW; Schema: backend; Owner: digitalspine
 --
 
 CREATE VIEW backend.smartplugview AS
@@ -199,52 +241,10 @@ CREATE VIEW backend.smartplugview AS
      JOIN backend."smartPlug" s ON ((s."applianceId" = a.id)));
 
 
-ALTER VIEW backend.smartplugview OWNER TO roy;
+ALTER VIEW backend.smartplugview OWNER TO digitalspine;
 
 --
--- Name: user_id_seq; Type: SEQUENCE; Schema: backend; Owner: roy
---
-
-CREATE SEQUENCE backend.user_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    MAXVALUE 2147483647
-    CACHE 1;
-
-
-ALTER SEQUENCE backend.user_id_seq OWNER TO roy;
-
---
--- Name: user; Type: TABLE; Schema: backend; Owner: roy
---
-
-CREATE TABLE backend."user" (
-    id integer DEFAULT nextval('backend.user_id_seq'::regclass) NOT NULL,
-    name character(255) NOT NULL,
-    password character(255) NOT NULL,
-    "devicePassword" character(255) NOT NULL
-);
-
-
-ALTER TABLE backend."user" OWNER TO roy;
-
---
--- Name: COLUMN "user".password; Type: COMMENT; Schema: backend; Owner: roy
---
-
-COMMENT ON COLUMN backend."user".password IS 'this is the password the user chooses to login in the web panel etc, is hashed salt and peppered etc';
-
-
---
--- Name: COLUMN "user"."devicePassword"; Type: COMMENT; Schema: backend; Owner: roy
---
-
-COMMENT ON COLUMN backend."user"."devicePassword" IS 'this is the one we send to the device to login, for now we store in clear, later will be encrypted (not hashed)';
-
-
---
--- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: roy
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: digitalspine
 --
 
 CREATE SEQUENCE public.user_id_seq
@@ -255,17 +255,25 @@ CREATE SEQUENCE public.user_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.user_id_seq OWNER TO roy;
+ALTER SEQUENCE public.user_id_seq OWNER TO digitalspine;
 
 --
--- Name: admin id; Type: DEFAULT; Schema: backend; Owner: roy
+-- Name: admin id; Type: DEFAULT; Schema: backend; Owner: digitalspine
 --
 
 ALTER TABLE ONLY backend.admin ALTER COLUMN id SET DEFAULT nextval('backend.admin_id_seq'::regclass);
 
 
 --
--- Data for Name: admin; Type: TABLE DATA; Schema: backend; Owner: roy
+-- Data for Name: account; Type: TABLE DATA; Schema: backend; Owner: digitalspine
+--
+
+COPY backend.account (id, name, password, "devicePassword") FROM stdin;
+\.
+
+
+--
+-- Data for Name: admin; Type: TABLE DATA; Schema: backend; Owner: digitalspine
 --
 
 COPY backend.admin (id, name, password, active) FROM stdin;
@@ -273,7 +281,7 @@ COPY backend.admin (id, name, password, active) FROM stdin;
 
 
 --
--- Data for Name: appliance; Type: TABLE DATA; Schema: backend; Owner: roy
+-- Data for Name: appliance; Type: TABLE DATA; Schema: backend; Owner: digitalspine
 --
 
 COPY backend.appliance (id, "accountId", serial, name, "deviceType", "applianceType", "lastIp", simulate, alreadymetered, lastupdate, notes) FROM stdin;
@@ -282,7 +290,7 @@ COPY backend.appliance (id, "accountId", serial, name, "deviceType", "applianceT
 
 
 --
--- Data for Name: smartPlug; Type: TABLE DATA; Schema: backend; Owner: roy
+-- Data for Name: smartPlug; Type: TABLE DATA; Schema: backend; Owner: digitalspine
 --
 
 COPY backend."smartPlug" ("applianceId", "maxPower", "minPower") FROM stdin;
@@ -291,43 +299,35 @@ COPY backend."smartPlug" ("applianceId", "maxPower", "minPower") FROM stdin;
 
 
 --
--- Data for Name: user; Type: TABLE DATA; Schema: backend; Owner: roy
---
-
-COPY backend."user" (id, name, password, "devicePassword") FROM stdin;
-\.
-
-
---
--- Name: admin_id_seq; Type: SEQUENCE SET; Schema: backend; Owner: roy
+-- Name: admin_id_seq; Type: SEQUENCE SET; Schema: backend; Owner: digitalspine
 --
 
 SELECT pg_catalog.setval('backend.admin_id_seq', 1, false);
 
 
 --
--- Name: appliance_id_seq; Type: SEQUENCE SET; Schema: backend; Owner: roy
+-- Name: appliance_id_seq; Type: SEQUENCE SET; Schema: backend; Owner: digitalspine
 --
 
 SELECT pg_catalog.setval('backend.appliance_id_seq', 1, true);
 
 
 --
--- Name: user_id_seq; Type: SEQUENCE SET; Schema: backend; Owner: roy
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: backend; Owner: digitalspine
 --
 
 SELECT pg_catalog.setval('backend.user_id_seq', 1, false);
 
 
 --
--- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: roy
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: digitalspine
 --
 
 SELECT pg_catalog.setval('public.user_id_seq', 1, false);
 
 
 --
--- Name: admin admin_pkey; Type: CONSTRAINT; Schema: backend; Owner: roy
+-- Name: admin admin_pkey; Type: CONSTRAINT; Schema: backend; Owner: digitalspine
 --
 
 ALTER TABLE ONLY backend.admin
@@ -335,7 +335,7 @@ ALTER TABLE ONLY backend.admin
 
 
 --
--- Name: appliance appliance_pkey; Type: CONSTRAINT; Schema: backend; Owner: roy
+-- Name: appliance appliance_pkey; Type: CONSTRAINT; Schema: backend; Owner: digitalspine
 --
 
 ALTER TABLE ONLY backend.appliance
@@ -343,19 +343,42 @@ ALTER TABLE ONLY backend.appliance
 
 
 --
--- Name: user user_name; Type: CONSTRAINT; Schema: backend; Owner: roy
+-- Name: appliance appliance_serial; Type: CONSTRAINT; Schema: backend; Owner: digitalspine
 --
 
-ALTER TABLE ONLY backend."user"
+ALTER TABLE ONLY backend.appliance
+    ADD CONSTRAINT appliance_serial UNIQUE (serial);
+
+
+--
+-- Name: smartPlug smartPlug_applianceId; Type: CONSTRAINT; Schema: backend; Owner: digitalspine
+--
+
+ALTER TABLE ONLY backend."smartPlug"
+    ADD CONSTRAINT "smartPlug_applianceId" PRIMARY KEY ("applianceId");
+
+
+--
+-- Name: account user_name; Type: CONSTRAINT; Schema: backend; Owner: digitalspine
+--
+
+ALTER TABLE ONLY backend.account
     ADD CONSTRAINT user_name UNIQUE (name);
 
 
 --
--- Name: user user_pkey; Type: CONSTRAINT; Schema: backend; Owner: roy
+-- Name: account user_pkey; Type: CONSTRAINT; Schema: backend; Owner: digitalspine
 --
 
-ALTER TABLE ONLY backend."user"
+ALTER TABLE ONLY backend.account
     ADD CONSTRAINT user_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: SEQUENCE user_id_seq; Type: ACL; Schema: public; Owner: digitalspine
+--
+
+GRANT ALL ON SEQUENCE public.user_id_seq TO digitalspine;
 
 
 --
